111ce60aefea64791f08f8e811e2fc2b41e8535d
王冲 <chongwang27@creditease.cn>
2020-09-08 13:22:05 +0800

    泛型的调整

--- JKSwiftExtension.podspec
+++ JKSwiftExtension.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'JKSwiftExtension'
-  s.version          = '0.0.2'
+  s.version          = '0.0.3'
   s.summary          = 'Swift版本的一个扩展'
 
 # This description is used to generate tags and improve search results.
--- JKSwiftExtension/Classes/JKSwiftExtesion/foundationExtension/Array+Extension.swift
+++ JKSwiftExtension/Classes/JKSwiftExtesion/foundationExtension/Array+Extension.swift
@@ -8,9 +8,11 @@
 import UIKit
 
 extension Array {
+    
     public subscript(secure index: Int) -> Element? {
         return indices ~= index ? self[index] : nil
     }
+    
     public mutating func append(_ elements: [Element]) {
         for e in elements {
             self.append(e)
@@ -18,6 +20,7 @@ extension Array {
     }
 }
 extension Array where Element : Equatable {
+    
     public mutating func remove(_ element: Element, isRepeat: Bool = true) {
         var removeIndexs: [Int] = []
         
@@ -34,6 +37,7 @@ extension Array where Element : Equatable {
             self.remove(at: index)
         }
     }
+    
     public mutating func removeArr(_ elements: [Element], isRepeat: Bool = true) {
         for e in elements {
             if self.contains(e) {
@@ -44,6 +48,7 @@ extension Array where Element : Equatable {
     
 }
 extension Array where Element : NSObjectProtocol {
+    
     public mutating func remove(_ object: NSObjectProtocol, isRepeat: Bool = true) {
         var removeIndexs: [Int] = []
         for i in 0 ..< count {
@@ -58,6 +63,7 @@ extension Array where Element : NSObjectProtocol {
             self.remove(at: index)
         }
     }
+    
     public mutating func removeArr(objects: [NSObjectProtocol], isRepeat: Bool = true) {
         for obj in objects {
             if self.contains(where: {$0.isEqual(obj)} ){
@@ -65,7 +71,6 @@ extension Array where Element : NSObjectProtocol {
             }
         }
     }
-    
 }
 
 public extension Array {
@@ -76,9 +81,9 @@ public extension Array {
         }
     }
     
-    @inlinable func compactMap<ElementOfResult>(_ transform: (Int, Element) throws -> ElementOfResult?) rethrows -> [ElementOfResult] {
+    @inlinable func compactMap<T>(_ transform: (Int, Element) throws -> T?) rethrows -> [T] {
         if count == 0 { return [] }
-        var newElements: [ElementOfResult] = []
+        var newElements: [T] = []
         for i in 0 ..< count {
             let newElement = try? transform(i, self[i])
             if let element = newElement {
@@ -87,7 +92,6 @@ public extension Array {
         }
         return newElements
     }
-    
     
     @inlinable func map<T>(_ transform: (Int, Element) throws -> T) rethrows -> [T] {
         if count == 0 { return [] }
