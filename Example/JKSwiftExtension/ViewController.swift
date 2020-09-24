2973389932756cf883f6ec4b87336a4196e7a5d2
王冲 <chongwang27@creditease.cn>
2020-09-24 16:46:42 +0800

    分支合并到 master

--- Example/JKSwiftExtension/ViewController.swift
+++ Example/JKSwiftExtension/ViewController.swift
@@ -14,14 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.edgesForExtendedLayout = []
-        self.view.backgroundColor = UIColor(hex: "#444444", alpha: 1.0)
+        self.view.backgroundColor = UIColor.c444444
         
-        let label1 = ActionLabel.createActionLabel(frame: CGRect(x: 16, y: 100, width: 100, height: 100), target: self, selector: #selector(tapAction1))
-        label1.backgroundColor = .brown
-        label1.action = { (label) in
-            print("哈哈1")
-        }
-        view.addSubview(label1)
         
     }
     
