0f90105ac184cd72171b242e9fb90c718251d8af
王冲 <chongwang27@creditease.cn>
2020-10-30 11:15:38 +0800

    Array 用例的完善

--- Example/JKSwiftExtension/Class/FoundationExtensionViewController/Controller/ArrayExtensionViewController.swift
+++ Example/JKSwiftExtension/Class/FoundationExtensionViewController/Controller/ArrayExtensionViewController.swift
@@ -8,41 +8,6 @@
 
 import UIKit
 
-//class ArrayExtensionViewController: UIViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        self.title = "Array+Extension"
-//        view.backgroundColor = .randomColor
-//
-//    }
-//
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        test()
-//    }
-//
-//    func test() {
-//        var array = Array(["1","2","1","3"])
-//        array.removeArr(["1","3"], isRepeat: true)
-//        print(array,"5")
-//        JKPrint("1", "3")
-//    }
-//
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//}
-
-
 class ArrayExtensionViewController: UIViewController {
     fileprivate static let ArrayExtensionViewControllerCellIdentifier = "ArrayExtensionViewControllerCellIdentifier"
     /// 资源数组
@@ -77,8 +42,8 @@ class ArrayExtensionViewController: UIViewController {
         self.edgesForExtendedLayout = []
         self.view.backgroundColor = UIColor.green
         
-        headDataArray = ["一、数组 的基本扩展", "二、数组 有关索引 的扩展方法"]
-        dataArray = [["安全的取某个索引的值"], ["获取数组中的指定元素的索引值", "获取元素首次出现的位置", "获取元素最后出现的位置"]]
+        headDataArray = ["一、数组 的基本扩展", "二、数组 有关索引 的扩展方法", "三、遵守 Equatable 协议的数组 (增删改查) 扩展", "四、遵守 NSObjectProtocol 协议对应数组的扩展方法", "五、针对数组元素是 String 的扩展"]
+        dataArray = [["安全的取某个索引的值", "数组新增元素(可转入一个数组)"], ["获取数组中的指定元素的索引值", "获取元素首次出现的位置", "获取元素最后出现的位置"], ["删除数组的中的元素(可删除第一个出现的或者删除全部出现的)", "从删除数组中删除一个数组中出现的元素，支持是否重复删除, 否则只删除第一次出现的元素"], ["删除数组中遵守NSObjectProtocol协议的元素，是否删除重复的元素", "删除一个遵守NSObjectProtocol的数组中的元素，支持重复删除"], ["数组转字符转（数组的元素是 字符串），如：[1, 2, 3] 连接器为 - ，那么转化后为 1-2-3"]]
         initUI()
     }
     
@@ -92,6 +57,59 @@ class ArrayExtensionViewController: UIViewController {
     }
 }
 
+// MARK:- 五、针对数组元素是 String 的扩展
+extension ArrayExtensionViewController {
+    
+    // MARK: 5.1、 数组转字符转（数组的元素是 字符串），如：["1", "2", "3"] 连接器为 - ，那么转化后为 "1-2-3"
+    @objc func test51() {
+        let testArray = ["1", "2", "3", "4", "5", "6"]
+        let testString = testArray.toStrinig(separator: "-")
+        JKPrint("数组转字符转（数组的元素是 字符串）", "数组：\(testArray) 转为字符串为：\(testString)")
+    }
+}
+// MARK:- 四、遵守 NSObjectProtocol 协议对应数组的扩展方法
+extension ArrayExtensionViewController {
+    
+    // MARK: 4.1、删除数组中遵守NSObjectProtocol协议的元素，是否删除重复的元素
+    @objc func test41() {
+        var testArray = ["1".toNSString, "2".toNSString, "3".toNSString, "2".toNSString]
+        let oldArray = testArray
+        let element = "2".toNSString
+        let newArray = testArray.remove(object: element, isRepeat: false)
+        JKPrint("删除数组中遵守NSObjectProtocol协议的元素，是否删除重复的元素)", "原数组为：\(oldArray) 删除其中的值：\(element) 后数组为：\(newArray)")
+    }
+    
+    // MARK: 4.2、删除一个遵守NSObjectProtocol的数组中的元素，支持重复删除
+    @objc func test42() {
+        var testArray = ["1".toNSString, "2".toNSString, "3".toNSString, "2".toNSString]
+        let removeArray = ["2".toNSString, "3".toNSString]
+        let oldArray = testArray
+        let newArray = testArray.removeArray(objects: removeArray, isRepeat: true)
+        JKPrint("删除一个遵守NSObjectProtocol的数组中的元素，支持重复删除", "原数组为：\(oldArray) 删除的数组是：\(removeArray) 后数组为：\(newArray)")
+    }
+}
+
+// MARK:- 三、遵守 Equatable 协议的数组 (增删改查) 扩展
+extension ArrayExtensionViewController {
+    
+    // MARK: 3.1、删除数组的中的元素(可删除第一个出现的或者删除全部出现的)
+    @objc func test31() {
+        var testArray = ["1", "2", "3", "2"]
+        let oldArray = testArray
+        let element = "2"
+        let newArray = testArray.remove(element, isRepeat: false)
+        JKPrint("删除数组的中的元素(可删除第一个出现的或者删除全部出现的)", "原数组为：\(oldArray) 删除其中的值：\(element) 后数组为：\(newArray)")
+    }
+    
+    // MARK: 3.2、从删除数组中删除一个数组中出现的元素，支持是否重复删除, 否则只删除第一次出现的元素
+    @objc func test32() {
+        var testArray = ["1", "2", "3", "2"]
+        let removeArray = ["2", "3"]
+        let oldArray = testArray
+        let newArray = testArray.removeArray(removeArray, isRepeat: false)
+        JKPrint("从删除数组中删除一个数组中出现的元素，支持是否重复删除, 否则只删除第一次出现的元素", "原数组为：\(oldArray) 删除的数组是：\(removeArray) 后数组为：\(newArray)")
+    }
+}
 // MARK:- 二、数组 有关索引 的扩展方法
 extension ArrayExtensionViewController {
     
@@ -125,11 +143,20 @@ extension ArrayExtensionViewController {
     @objc func test11() {
         let testArray = ["1", "2", "3", "2"]
         let index = 2
-        
         guard let value = testArray.indexValue(safe: index) else {
+            JKPrint("取数组 \(testArray) 索引为 \(index) 是没有值的")
             return
         }
         JKPrint("安全的取某个索引的值", "在数组 \(testArray) 中获取索引 \(index) 的值是 \(value)")
+    }
+    
+    // MARK: 1.2、数组新增元素(可转入一个数组)
+    @objc func test12() {
+        var testArray = ["1", "2", "3"]
+        let addArray = ["4", "5", "6"]
+        let oldArray = testArray
+        testArray.append(addArray)
+        JKPrint("数组新增元素(可转入一个数组)", "原数组是： \(oldArray) 添加 \(addArray) 后为 \(testArray)")
     }
 
 }
--- JKSwiftExtension/Classes/JKSwiftExtesion/FoundationExtension/Array+Extension.swift
+++ JKSwiftExtension/Classes/JKSwiftExtesion/FoundationExtension/Array+Extension.swift
@@ -15,8 +15,17 @@ public extension Array {
     func indexValue(safe index: Index) -> Element? {
         return indices.contains(index) ? self[index] : nil
     }
-}
     
+    // MARK: 1.2、数组新增元素(可转入一个数组)
+    /// 数组新增元素(可转入一个数组)
+    /// - Parameter elements: 数组
+    mutating func append(_ elements: [Element]) {
+        for element in elements {
+            self.append(element)
+        }
+    }
+}
+
 // MARK:- 二、数组 有关索引 的扩展方法
 public extension Array where Element : Equatable {
     
@@ -53,28 +62,16 @@ public extension Array where Element : Equatable {
     }
 }
 
-// MARK:- 三、数组有关 (增删改查) 的的扩展方法
-public extension Array {
-    
-    // MARK: 数组新增元素(可转入一个数组)
-    /// 数组新增元素(可转入一个数组)
-    /// - Parameter elements: 数组
-    mutating func append(_ elements: [Element]) {
-        for element in elements {
-            self.append(element)
-        }
-    }
-}
-
-// MARK:- 遵守 Equatable 协议的数组
+// MARK:- 三、遵守 Equatable 协议的数组 (增删改查) 扩展
 public extension Array where Element : Equatable {
     
-    // MARK: 删除数组的中的元素
-    /// 删除数组的中的元素
+    // MARK: 3.1、删除数组的中的元素(可删除第一个出现的或者删除全部出现的)
+    /// 删除数组的中的元素(可删除第一个出现的或者删除全部出现的)
     /// - Parameters:
     ///   - element: 要删除的元素
     ///   - isRepeat: 是否删除重复的元素
-    mutating func remove(_ element: Element, isRepeat: Bool = true) {
+    @discardableResult
+    mutating func remove(_ element: Element, isRepeat: Bool = true) -> Array {
         var removeIndexs: [Int] = []
         
         for i in 0 ..< count {
@@ -87,44 +84,37 @@ public extension Array where Element : Equatable {
         for index in removeIndexs.reversed() {
             self.remove(at: index)
         }
+        return self
     }
     
-    // MARK: 删除一个数组
-    /// 删除一个数组
+    // MARK: 3.2、从删除数组中删除一个数组中出现的元素，支持是否重复删除, 否则只删除第一次出现的元素
+    /// 从删除数组中删除一个数组中出现的元素，支持是否重复删除, 否则只删除第一次出现的元素
     /// - Parameters:
-    ///   - elements: 对应的数组
+    ///   - elements: 被删除的数组元素
     ///   - isRepeat: 是否删除重复的元素
-    mutating func removeArr(_ elements: [Element], isRepeat: Bool = true) {
+    @discardableResult
+    mutating func removeArray(_ elements: [Element], isRepeat: Bool = true) -> Array {
         for element in elements {
             if self.contains(element) {
-                self.remove(element)
+                self.remove(element, isRepeat: isRepeat)
             }
         }
+        return self
     }
 }
 
-// MARK:- 针对数组元素是 String 的扩展
-public extension Array where Self.Element == String {
-  
-    // MARK: 1.1、数组转字符转（数组的元素是 字符串）
-    /// 数组转字符转（数组的元素是 字符串）
-    /// - Returns: 字符串
-    func toStrinig(separator: String = "") -> String {
-        return self.joined(separator: separator)
-    }
-}
-
-// MARK:- 遵守 NSObjectProtocol 协议对应数组的方法
+// MARK:- 四、遵守 NSObjectProtocol 协议对应数组的扩展方法
 public extension Array where Element : NSObjectProtocol {
     
-    // MARK: 移除数组中遵守NSObjectProtocol协议的元素
-    /// 移除数组中遵守NSObjectProtocol协议的元素
+    // MARK: 4.1、删除数组中遵守NSObjectProtocol协议的元素，是否删除重复的元素
+    /// 删除数组中遵守NSObjectProtocol协议的元素
     /// - Parameters:
     ///   - object: 元素
     ///   - isRepeat: 是否删除重复的元素
-    mutating func remove(_ object: NSObjectProtocol, isRepeat: Bool = true) {
+    @discardableResult
+    mutating func remove(object: NSObjectProtocol, isRepeat: Bool = true) -> Array {
         var removeIndexs: [Int] = []
-        for i in 0 ..< count {
+        for i in 0..<count {
             if self[i].isEqual(object) {
                 removeIndexs.append(i)
                 if !isRepeat {
@@ -135,19 +125,33 @@ public extension Array where Element : NSObjectProtocol {
         for index in removeIndexs.reversed() {
             self.remove(at: index)
         }
+        return self
     }
     
-    // MARK: 删除一个遵守NSObjectProtocol的数组
-    /// 删除一个遵守NSObjectProtocol的数组
+    // MARK: 4.2、删除一个遵守NSObjectProtocol的数组中的元素，支持重复删除
+    /// 删除一个遵守NSObjectProtocol的数组中的元素，支持重复删除
     /// - Parameters:
     ///   - objects: 遵守NSObjectProtocol的数组
     ///   - isRepeat: 是否删除重复的元素
-    mutating func removeArr(objects: [NSObjectProtocol], isRepeat: Bool = true) {
+    @discardableResult
+    mutating func removeArray(objects: [NSObjectProtocol], isRepeat: Bool = true) -> Array {
         for object in objects {
             if self.contains(where: {$0.isEqual(object)} ){
-                self.remove(object)
+                self.remove(object: object, isRepeat: isRepeat)
             }
         }
+        return self
     }
 }
 
+// MARK:- 五、针对数组元素是 String 的扩展
+public extension Array where Self.Element == String {
+    
+    // MARK: 5.1、数组转字符转（数组的元素是 字符串），如：["1", "2", "3"] 连接器为 - ，那么转化后为 "1-2-3"
+    /// 数组转字符转（数组的元素是 字符串），如：["1", "2", "3"] 连接器为 - ，那么转化后为 "1-2-3"
+    /// - Parameter separator: 连接器
+    /// - Returns: 转化后的字符串
+    func toStrinig(separator: String = "") -> String {
+        return self.joined(separator: separator)
+    }
+}
