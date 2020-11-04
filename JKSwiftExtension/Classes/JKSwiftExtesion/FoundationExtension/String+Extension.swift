a8e6913c6174060f2836636a2929859d858bec84
王冲 <chongwang27@creditease.cn>
2020-11-04 16:25:13 +0800

    String+Extension 新增 进制转换

--- JKSwiftExtension/Classes/JKSwiftExtesion/FoundationExtension/String+Extension.swift
+++ JKSwiftExtension/Classes/JKSwiftExtesion/FoundationExtension/String+Extension.swift
@@ -1186,7 +1186,19 @@ extension String {
  */
 public extension String {
     
-    // MARK: 11.1、二进制 -> 十进制
+    // MARK: 11.1、二进制 转 八进制
+    /// 二进制 转 八进制
+    /// - Returns: 八进制
+    func binaryToOctal() -> String {
+        // 二进制
+        let binary = self
+        // 十进制
+        let decimal = binary.binaryTodecimal()
+        // 八进制
+        return decimal.decimalToOctal()
+    }
+    
+    // MARK: 11.2、二进制 -> 十进制
     /// 二进制 -> 十进制
     /// - Returns: 十进制
     func binaryTodecimal() -> String {
@@ -1200,7 +1212,31 @@ public extension String {
         return "\(sum)"
     }
     
-    // MARK: 11.2、八进制 -> 十进制
+    // MARK: 11.3、二进制 转 十六进制
+    /// 二进制 转 十六进制
+    /// - Returns: 十六进制
+    func binaryToHexadecimal() -> String {
+        // 二进制
+        let binary = self
+        // 十进制
+        let decimal = binary.binaryTodecimal()
+        // 十六进制
+        return decimal.decimalToHexadecimal()
+    }
+    
+    // MARK: 11.4、八进制 -> 二进制
+    /// 八进制 -> 二进制
+    /// - Returns: 二进制
+    func octalTobinary() -> String {
+        // 八进制
+        let octal = self
+        // 十进制
+        let decimal = octal.octalTodecimal()
+        // 二进制
+        return decimal.decimalToBinary()
+    }
+    
+    // MARK: 11.5、八进制 -> 十进制
     /// 八进制 -> 十进制
     /// - Returns: 十进制
     func octalTodecimal() -> String {
@@ -1214,7 +1250,89 @@ public extension String {
         return "\(sum)"
     }
     
-    // MARK: 11.3、十六进制 转 十进制
+    // MARK: 11.6、八进制 -> 十六进制
+    /// 八进制 -> 十六进制
+    /// - Returns: 十六进制
+    func octalToHexadecimal() -> String {
+        // 八进制
+        let octal = self
+        // 十进制
+        let decimal = octal.octalTodecimal()
+        // 十六进制
+        return decimal.decimalToHexadecimal()
+    }
+    
+    // MARK: 11.7、十进制 转 二进制
+    /// 十进制 转 二进制
+    /// - Returns: 二进制
+    func decimalToBinary() -> String {
+        guard var decimal = self.toInt() else {
+            return ""
+        }
+        var str = ""
+        while decimal > 0 {
+            str = "\(decimal % 2)" + str
+            decimal /= 2
+        }
+        return str
+    }
+    
+    // MARK: 11.8、十进制转 八进制
+    /// 十进制转 八进制
+    /// - Returns: 八进制
+    func decimalToOctal() -> String {
+        guard let decimal = self.toInt() else {
+           return ""
+       }
+       /*
+       guard var decimal = self.toInt() else {
+         return ""
+       }
+       var str = ""
+       while decimal > 0 {
+           str = "\(decimal % 8)" + str
+           decimal /= 8
+       }
+       return str
+      */
+       return String(format: "%0O", decimal)
+    }
+    
+    // MARK: 11.9、十进制转 十六进制
+    /// 十进制转 十六进制
+    /// - Returns: 十六进制
+    func decimalToHexadecimal() -> String {
+       guard let decimal = self.toInt() else {
+           return ""
+       }
+       return String(format: "%0X", decimal)
+    }
+    
+    // MARK: 11.10、十六进制 转 二进制
+    /// 十六进制  转 二进制
+    /// - Returns: 二进制
+    func hexadecimalToBinary() -> String {
+        // 十六进制
+        let hexadecimal = self
+        // 十进制
+        let decimal = hexadecimal.hexadecimalToDecimal()
+        // 二进制
+        return decimal.decimalToBinary()
+    }
+    
+    // MARK: 11.11、十六进制 转 八进制
+    /// 十六进制  转 八进制
+    /// - Returns: 八进制
+    func hexadecimalToOctal() -> String {
+        // 十六进制
+        let hexadecimal = self
+        // 十进制
+        let decimal = hexadecimal.hexadecimalToDecimal()
+        // 八进制
+        return decimal.decimalToOctal()
+    }
+    
+    // MARK: 11.12、十六进制 转 十进制
     /// 十六进制  转 十进制
     /// - Returns: 十进制
     func hexadecimalToDecimal() -> String {
@@ -1229,44 +1347,5 @@ public extension String {
             }
         }
         return "\(sum)"
-    }
-    
-    // MARK: 11.4、十进制 转 二进制
-    /// 十进制 转 二进制
-    /// - Returns: 二进制
-    func decimalToBinary() -> String {
-        guard var decimal = self.toInt() else {
-            return ""
-        }
-        var str = ""
-        while decimal > 0 {
-            str = "\(decimal % 2)" + str
-            decimal /= 2
-        }
-        return str
-    }
-    // MARK: 11.5、十进制转 八进制
-    /// 十进制转 八进制
-    /// - Returns: 八进制
-    func decimalToOctal() -> String {
-       guard var decimal = self.toInt() else {
-           return ""
-       }
-       var str = ""
-       while decimal > 0 {
-           str = "\(decimal % 8)" + str
-           decimal /= 8
-       }
-       return str
-    }
-    
-    // MARK: 11.6、十进制转 十六进制
-    /// 十进制转 十六进制
-    /// - Returns: 十六进制
-    func decimalToHexadecimal() -> String {
-       guard let decimal = self.toInt() else {
-           return ""
-       }
-       return String(format: "%0X", decimal)
     }
 }
