a1219f0857b505ad7db40421680be0866ffdc7b5
王冲 <chongwang27@creditease.cn>
2021-04-06 18:06:16 +0800

    UITextView 限制行数、限制字符、自动变化高度的去除

--- Example/JKSwiftExtension/Class/UIKitExtensionViewController/Controller/UITextViewExtensionViewController.swift
+++ Example/JKSwiftExtension/Class/UIKitExtensionViewController/Controller/UITextViewExtensionViewController.swift
@@ -14,7 +14,7 @@ class UITextViewExtensionViewController: BaseViewController {
         super.viewDidLoad()
         
         headDataArray = ["一、基本的扩展 (使用runtime添加属性)", "二、文本链接的扩展"]
-        dataArray = [["增加占位符", "限制的字数", "限制行数", "默认文本字体的大小", "默认文本的颜色", "设置 默认文本的Origin", "是否自动变化高度"], ["添加链接文本（链接为空时则表示普通文本）", "转换特殊符号标签字段"]]
+        dataArray = [["增加占位符", "默认文本字体的大小", "默认文本的颜色", "设置 默认文本的Origin"], ["添加链接文本（链接为空时则表示普通文本）", "转换特殊符号标签字段"]]
     }
     
     deinit {
@@ -115,27 +115,8 @@ extension UITextViewExtensionViewController: UITextViewDelegate {
 // MARK:- 一、基本的扩展
 extension UITextViewExtensionViewController {
     
-    // MARK: 1.7、是否自动变化高度
-    @objc func test17() {
-        let textView = UITextView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
-        textView.backgroundColor = .brown
-        textView.textColor = .white
-        textView.placeholdFont = UIFont.systemFont(ofSize: 16)
-        textView.placeholdColor = .green
-        textView.placeholderOrigin = CGPoint(x: 1, y: 7)
-        textView.autoHeight = true
-        textView.font = UIFont.systemFont(ofSize: 16)
-        textView.placeholder = "设置默认文本的CGSize(width: 20, height: 20)"
-        self.view.addSubview(textView)
-        JKPrint("设置默认文本的CGSize(width: 20, height: 20)")
-        JKAsyncs.asyncDelay(100, {
-        }) {
-            textView.removeFromSuperview()
-        }
-    }
-    
-    // MARK: 1.6、设置默认文本的 CGSize
-    @objc func test16() {
+    // MARK: 1.4、设置默认文本的 CGPoint
+    @objc func test14() {
         let textView = UITextView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
         textView.backgroundColor = .brown
         textView.textColor = .white
@@ -152,8 +133,8 @@ extension UITextViewExtensionViewController {
         }
     }
     
-    // MARK: 1.5、默认文本的颜色
-    @objc func test15() {
+    // MARK: 1.3、默认文本的颜色
+    @objc func test13() {
         let textView = UITextView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
         textView.backgroundColor = .brown
         textView.textColor = .white
@@ -169,52 +150,17 @@ extension UITextViewExtensionViewController {
         }
     }
     
-    // MARK: 1.4、默认文本字体的大小
-    @objc func test14() {
+    // MARK: 1.2、默认文本字体的大小
+    @objc func test12() {
         let textView = UITextView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+        textView.font = UIFont.systemFont(ofSize: 16)
         textView.backgroundColor = .brown
         textView.textColor = .white
-        textView.placeholder = "默认文本字体的大小：16"
+        textView.placeholder = "默认文本字体的大小：16默认文本字体的大小：16"
         textView.placeholdFont = UIFont.systemFont(ofSize: 16)
-        textView.font = UIFont.systemFont(ofSize: 16)
         self.view.addSubview(textView)
         textView.placeholdColor = .yellow
         JKPrint("默认文本字体的大小")
-        JKAsyncs.asyncDelay(8, {
-        }) {
-            textView.removeFromSuperview()
-        }
-    }
-    
-    // MARK: 1.3、限制行数
-    @objc func test13() {
-        let textView = UITextView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
-        textView.backgroundColor = .brown
-        textView.textColor = .white
-        textView.font = UIFont.systemFont(ofSize: 12)
-        textView.limitLines = 20
-        textView.placeholder = "我限制行数：1"
-        textView.limitLines = 1
-        self.view.addSubview(textView)
-        JKPrint("设置占位符")
-        JKAsyncs.asyncDelay(8, {
-        }) {
-            textView.removeFromSuperview()
-        }
-    }
-    
-    // MARK: 1.2、限制的字数
-    @objc func test12() {
-        let textView = UITextView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
-        textView.backgroundColor = .brown
-        textView.textColor = .white
-        textView.font = UIFont.systemFont(ofSize: 12)
-        textView.placeholder = "限制的字数: 5"
-        textView.limitLength = 5
-        self.view.addSubview(textView)
-        
-        JKPrint("限制的字数: 5")
-        
         JKAsyncs.asyncDelay(8, {
         }) {
             textView.removeFromSuperview()
--- JKSwiftExtension/Classes/JKSwiftExtesion/UIKitExtension/UITextView+Extension.swift
+++ JKSwiftExtension/Classes/JKSwiftExtesion/UIKitExtension/UITextView+Extension.swift
@@ -23,31 +23,7 @@ public extension UITextView {
         }
     }
     
-    // MARK: 1.2、限制的字数
-    /// 限制的字数
-    var limitLength: NSNumber? {
-        set {
-            objc_setAssociatedObject(self, UITextView.RuntimeKey.limitLength, newValue, .OBJC_ASSOCIATION_COPY_NONATOMIC)
-            initWordCountLabel(limitLength!)
-        }
-        get {
-            return objc_getAssociatedObject(self, UITextView.RuntimeKey.limitLength) as? NSNumber
-        }
-    }
-    
-    // MARK: 1.3、限制行数
-    /// 限制的行数
-    var limitLines: NSNumber? {
-        set {
-            objc_setAssociatedObject(self, UITextView.RuntimeKey.limitLines, newValue, .OBJC_ASSOCIATION_COPY_NONATOMIC)
-            NotificationCenter.default.addObserver(self, selector: #selector(limitLengthEvent), name: UITextView.textDidChangeNotification, object: self)
-        }
-        get {
-            return objc_getAssociatedObject(self, UITextView.RuntimeKey.limitLines) as? NSNumber
-        }
-    }
-    
-    // MARK: 1.4、默认文本字体的大小
+    // MARK: 1.2、默认文本字体的大小
     /// 默认文本字体的大小
     var placeholdFont: UIFont? {
         set {
@@ -61,7 +37,7 @@ public extension UITextView {
         }
     }
     
-    // MARK: 1.5、默认文本的颜色
+    // MARK: 1.3、默认文本的颜色
     /// 默认文本的颜色
     var placeholdColor: UIColor? {
         set {
@@ -75,7 +51,7 @@ public extension UITextView {
         }
     }
     
-    // MARK: 1.6、设置 默认文本的Origin
+    // MARK: 1.4、设置 默认文本的Origin
     /// 设置 默认文本的Origin
     var placeholderOrigin: CGPoint? {
         set {
@@ -88,55 +64,18 @@ public extension UITextView {
             return objc_getAssociatedObject(self, UITextView.RuntimeKey.placeholderOrigin) as? CGPoint == nil ? CGPoint(x: 7, y: 7) : objc_getAssociatedObject(self, UITextView.RuntimeKey.placeholderOrigin) as? CGPoint
         }
     }
-    
-    // MARK: 1.7、是否自动变化高度
-    /// 是否自动变化高度
-    var autoHeight: Bool? {
-        set {
-            objc_setAssociatedObject(self, UITextView.RuntimeKey.autoHeight, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
-        get {
-            return objc_getAssociatedObject(self, UITextView.RuntimeKey.autoHeight) as? Bool == nil ? false : objc_getAssociatedObject(self, UITextView.RuntimeKey.autoHeight) as? Bool
-        }
-    }
 }
 
 // MARK:- fileprivate 私有的内容
 extension UITextView {
     
     fileprivate struct RuntimeKey {
-        static let placeholder: UnsafeRawPointer! = UnsafeRawPointer.init(bitPattern: "PLACEHOLDEL".hashValue)
-        static let limitLength: UnsafeRawPointer! = UnsafeRawPointer.init(bitPattern: "LIMITLENGTH".hashValue)
-        static let limitLines: UnsafeRawPointer! = UnsafeRawPointer.init(bitPattern: "LIMITLINES".hashValue)
-        static let placeholderLabel: UnsafeRawPointer! = UnsafeRawPointer.init(bitPattern: "PLACEHOLDELABEL".hashValue)
-        static let wordCountLabel: UnsafeRawPointer! = UnsafeRawPointer.init(bitPattern: "WORDCOUNTLABEL".hashValue)
-        static let placeholdFont: UnsafeRawPointer! = UnsafeRawPointer.init(bitPattern: "PLACEHOLDFONT".hashValue)
-        static let placeholdColor: UnsafeRawPointer! = UnsafeRawPointer.init(bitPattern: "PLACEHOLDCOLOR".hashValue)
-        static let limitLabelColor: UnsafeRawPointer! = UnsafeRawPointer.init(bitPattern: "LIMITLABLECOLOR".hashValue)
-        static let autoHeight: UnsafeRawPointer! = UnsafeRawPointer.init(bitPattern: "AUTOHEIGHT".hashValue)
-        static let oldFrame: UnsafeRawPointer! = UnsafeRawPointer.init(bitPattern: "LODFRAME".hashValue)
-        static let placeholderOrigin: UnsafeRawPointer! = UnsafeRawPointer.init(bitPattern: "PLACEHOLDERORIGIN".hashValue)
+        static let placeholder: UnsafeRawPointer! = UnsafeRawPointer(bitPattern: "PLACEHOLDEL".hashValue)
+        static let placeholderLabel: UnsafeRawPointer! = UnsafeRawPointer(bitPattern: "PLACEHOLDELABEL".hashValue)
+        static let placeholdFont: UnsafeRawPointer! = UnsafeRawPointer(bitPattern: "PLACEHOLDFONT".hashValue)
+        static let placeholdColor: UnsafeRawPointer! = UnsafeRawPointer(bitPattern: "PLACEHOLDCOLOR".hashValue)
+        static let placeholderOrigin: UnsafeRawPointer! = UnsafeRawPointer(bitPattern: "PLACEHOLDERORIGIN".hashValue)
         // ...其他Key声明
-    }
-    
-    /// 最初的 frame
-    fileprivate var oldFrame: CGRect? {
-        set {
-            objc_setAssociatedObject(self, UITextView.RuntimeKey.oldFrame, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
-        get {
-            return objc_getAssociatedObject(self, UITextView.RuntimeKey.oldFrame) as? CGRect
-        }
-    }
-    
-    /// 文字数量
-    fileprivate var wordCountLabel: UILabel? {
-        set {
-            objc_setAssociatedObject(self, UITextView.RuntimeKey.wordCountLabel, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
-        get {
-            return objc_getAssociatedObject(self, UITextView.RuntimeKey.wordCountLabel) as? UILabel
-        }
     }
     
     /// 默认文本
@@ -154,116 +93,29 @@ extension UITextView {
     fileprivate func initPlaceholder(_ placeholder: String) {
         
         NotificationCenter.default.addObserver(self, selector: #selector(textChange(_:)), name: UITextView.textDidChangeNotification, object: self)
-        self.placeholderLabel = UILabel()
-        placeholderLabel?.font = self.placeholdFont
-        placeholderLabel?.text = placeholder
-        placeholderLabel?.backgroundColor = .randomColor
-        placeholderLabel?.numberOfLines = 0
-        placeholderLabel?.lineBreakMode = .byWordWrapping
-        placeholderLabel?.textColor = self.placeholdColor
-        let rect = placeholder.boundingRect(with: CGSize(width: self.jk.width - placeholderOrigin!.x, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font : self.placeholdFont!], context: nil)
-        placeholderLabel?.frame = CGRect(x: placeholderOrigin!.x + 1, y: placeholderOrigin!.y, width: self.jk.width - placeholderOrigin!.x * 2 - 1, height: rect.size.height)
+        let label = UILabel()
+        let width = self.jk.width - placeholderOrigin!.x * 2 - 1
+        let rect = placeholder.boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font : self.placeholdFont!], context: nil)
+        label.frame = CGRect(x: placeholderOrigin!.x + 1, y: placeholderOrigin!.y, width: width, height: rect.size.height + 8)
+        label.font = self.placeholdFont
+        label.text = placeholder
+        label.numberOfLines = 0
+        // label.lineBreakMode = .byWordWrapping
+        label.textColor = self.placeholdColor
+        label.backgroundColor = .randomColor
+        self.placeholderLabel = label
         addSubview(self.placeholderLabel!)
-        oldFrame = self.frame
         placeholderLabel?.isHidden = self.text.count > 0 ? true : false
-    }
-    
-    ///  字数限制
-    /// - Parameter limitLength:  字数限制
-    fileprivate func initWordCountLabel(_ limitLength : NSNumber) {
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(limitLengthEvent), name: UITextView.textDidChangeNotification, object: self)
-        if wordCountLabel != nil {
-            wordCountLabel?.removeFromSuperview()
-        }
-        wordCountLabel = UILabel(frame: CGRect(x: 0, y: self.frame.size.height - 20, width: self.frame.size.width - 10, height: 20))
-        wordCountLabel?.textAlignment = .right
-        wordCountLabel?.textColor = self.textColor
-        wordCountLabel?.font = self.font
-        if self.text.count > limitLength.intValue {
-            self.text = (self.text as NSString).substring(to: limitLength.intValue)
-        }
-        wordCountLabel?.text = "\(self.text.count)/\(limitLength)"
-        addSubview(wordCountLabel!)
-        self.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 20, right: 0)
     }
     
     /// 动态监听
     /// - Parameter notification: 动态监听
     @objc fileprivate func textChange(_ notification : Notification) {
         let textView = notification.object as! UITextView;
-        self.text = textView.text;
         if placeholder != nil {
             placeholderLabel?.isHidden = true
-            if self.text.count ==  0 {
-                self.placeholderLabel?.isHidden = false
-            }
+            self.placeholderLabel?.isHidden = !(textView.text.count == 0)
         }
-        if limitLength != nil {
-            var wordCount = self.text.count
-            if wordCount > (limitLength?.intValue)! {
-                wordCount = (limitLength?.intValue)!
-            }
-            let limit = limitLength!.stringValue
-            wordCountLabel?.text = "\(wordCount)/\(limit)"
-        }
-        if autoHeight == true {
-            let size = getStringPlaceSize(self.text, textFont: self.font!)
-            UIView.animate(withDuration: 0.15) { [weak self] in
-                guard let weakSelf = self else { return }
-                // JKPrint("autoHeight的CGPoint=========\(weakSelf.placeholderOrigin!)")
-                weakSelf.frame = CGRect(x: weakSelf.jk.x, y: weakSelf.jk.y, width: weakSelf.jk.width, height: size.height + weakSelf.placeholderOrigin!.y * 2 < weakSelf.oldFrame!.height ? weakSelf.oldFrame!.height : size.height + weakSelf.placeholderOrigin!.y * 2)
-            }
-        }
-    }
-    
-    @objc fileprivate func limitLengthEvent() {
-        if limitLength != nil {
-            if self.text.count > (limitLength?.intValue)! && limitLength != nil {
-                self.text = (self.text as NSString).substring(to: (limitLength?.intValue)!)
-                // JKPrint("Maximum number of words");
-            }
-        } else {
-            // 行数限制
-            if (limitLines != nil) {
-                var size = getStringPlaceSize(self.text, textFont: self.font!)
-                let height = self.font!.lineHeight * CGFloat(limitLines!.floatValue)
-                if (size.height > height) {
-                    // JKPrint("Maximum number of lines");
-                    //循环计算当前高度可以存放的字符
-                    while size.height > height {
-                        self.text = (self.text as NSString).substring(to: self.text.count - 1)
-                        size = getStringPlaceSize(self.text, textFont: self.font!)
-                    }
-                }
-            }
-        }
-    }
-    
-    @objc fileprivate func getStringPlaceSize(_ string : String, textFont : UIFont) -> CGSize {
-        ///计算文本高度
-        let font : UIFont = textFont
-        let attribute = [NSAttributedString.Key.font : font];
-        let options = NSStringDrawingOptions.usesLineFragmentOrigin
-        let size = string.boundingRect(with: CGSize(width: self.contentSize.width - 10, height: CGFloat.greatestFiniteMagnitude), options: options, attributes: attribute, context: nil).size
-        return size
-    }
-    
-    open override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-        if action == #selector(UIResponderStandardEditActions.paste(_:))
-            || action == #selector(UIResponderStandardEditActions.select(_:))
-            || action == #selector(UIResponderStandardEditActions.selectAll(_:))
-            || action == #selector(UIResponderStandardEditActions.copy(_:))
-            || action == #selector(UIResponderStandardEditActions.cut(_:))
-            || action == #selector(UIResponderStandardEditActions.delete(_:))
-        {
-            OperationQueue.main.addOperation {
-                UIMenuController.shared.setMenuVisible(false, animated: false)
-            }
-            self.selectedRange = NSRange.init(location: 0, length: 0)
-            return false
-        }
-        return super.canPerformAction(action, withSender: sender)
     }
 }
 
