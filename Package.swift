90eb5bd947a3a984aee24265c702aaafeaafba0e
王冲 <chongwang27@creditease.cn>
2021-12-23 18:42:39 +0800

    Swift Package 调整

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
 // swift-tools-version:5.5
+
 import PackageDescription
 
 let package = Package(
     name: "JKSwiftExtension",
-    platforms: [.iOS(.v9.0)],
+    platforms: [.iOS(.v9)],
     products: [
         .library(name: "JKSwiftExtension", targets: ["JKSwiftExtension"])
     ],
