51fb9bad11d884a8c98eee691508083905bbc624
王冲 <chongwang27@creditease.cn>
2021-12-24 14:17:03 +0800

    SPM增加最低9.0系统限制

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "JKSwiftExtension",
+    platforms: [.iOS(.v9)],
     products: [
         .library(
             name: "JKSwiftExtension",
